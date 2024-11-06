9adeb686d8503e44f415ffc81fc3391ee2df9802
Maksim Antoshkin <dre.maks474@mail.ru>
2024-11-06 10:10:43 +0300

    Project created

---
+++ src/main/java/com/project/kursach/KursachApplication.java
@@ -0,0 +1,13 @@
+package com.project.kursach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursachApplication.class, args);
+	}
+
+}
